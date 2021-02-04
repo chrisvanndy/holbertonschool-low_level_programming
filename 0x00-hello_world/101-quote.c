704874d4218769ed1737e46baa55409d015a05a6
chrisvanndy <chrisvannyc@gmail.com>
2021-02-04 02:54:18 +0000

    char *string

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#includ < stdio.h >
+/**
+ * main - my function
+ *
+ * this program writes a quote to an error message
+ * Return: 1
+ */
+int main(void)
+{
+	char *s = "and that piece of art is useful\" \- Dora Korpar, 2015-10-19\n";
+
+	while (*s) putchar(*s++);
+
+	
+	return (1);
+}
