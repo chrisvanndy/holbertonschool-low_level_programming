f54b4150eebd79692efd314bc4a0efdbd4957daa
chrisvanndy <chrisvannyc@gmail.com>
2021-02-04 03:44:43 +0000

    58

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	if (write(1, "This will be output to standard out\n", 36) != 36)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 57);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (-1);
 }
 
