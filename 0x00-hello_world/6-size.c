cdbcadb1281145b2f3b304a99e3c64cc7c329df4
chrisvanndy <chrisvannyc@gmail.com>
2021-02-04 00:24:05 +0000

    %lu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ long int li;
 long long int lli;
 float f;
 
-printf("Size of a char: %i byte(s)\n", (int)sizeof(c));
-printf("Size of an int: %i bytes(s)\n", (int)sizeof(i));
-printf("Size of a long int: %i bytes(s)\n", (int)sizeof(li));
-printf("Size of a long long int: %i byte(s)\n", (int)sizeof(lli));
-printf("Size of a float: %i byte(s)\n", (int)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(li));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 return (0);
 }
