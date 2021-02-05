49ed8b9b9eb718c49119acf67b5d9d346c7e7fb1
chrisvanndy <chrisvannyc@gmail.com>
2021-02-05 21:35:10 +0000

    betty style

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - my function
  * I have no idea what to do
@@ -10,12 +11,12 @@ int main(void)
 	int x;
 
 	for (x = '0'; x <= '9'; x++)
-{	
+	{	
 		putchar (x);
 	if (x < '9')
 		putchar(',');
 		putchar(' ');
-}
+	}
 	putchar('\n');
 	return (0);
 }
