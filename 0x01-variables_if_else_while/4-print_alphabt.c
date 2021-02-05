5864cf42f2c035889ff0f0218a320d7665bc8a17
chrisvanndy <chrisvannyc@gmail.com>
2021-02-05 03:13:48 +0000

    true true print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * Main - this is my function
+ * putchar and omit q,e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+{
+	if (c != 'q' && c != 'e')
+	putchar (c);
+}
+        putchar ('\n');
+        return (0);
+} 
