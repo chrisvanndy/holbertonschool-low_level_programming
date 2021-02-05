7e3965825dd7ec6599667a9bcd9486721985bbb9
chrisvanndy <chrisvannyc@gmail.com>
2021-02-05 00:17:12 +0000

    a/a-z/Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ * Main - this is my function
+ * int c,v - lowercase and capital variables
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c, v;
+{
+	for (c = 'a'; c <= 'z'; c++)
+	putchar (c);
+}
+{
+	for (v = 'A'; v <= 'Z'; v++)
+	putchar (v);
+	putchar ('\n');
+}
+	return (0);
+}
