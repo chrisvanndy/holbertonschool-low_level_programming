91bbd3d63569542e9dae29aa47079ccec11a9469
chrisvanndy <chrisvannyc@gmail.com>
2021-02-05 01:35:51 +0000

    print to base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * main - this is my function
+ * while statement base 10 numbers
+ *
+ * return: 0
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+{
+	printf("%d\n", x);
+	x = x + 1;
+}
+}
