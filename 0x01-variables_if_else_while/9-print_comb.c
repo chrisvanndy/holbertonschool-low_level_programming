775c85fb59619935b7648e7e7ef7c755ba879bf8
chrisvanndy <chrisvannyc@gmail.com>
2021-02-05 21:16:53 +0000

    curly braces effect the loops

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - my function
+ * I have no idea what to do
+ *
+ * Return 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+{	
+		putchar (x);
+	if (x < '9')
+		putchar(',');
+		putchar(' ');
+}
+	putchar('\n');
+	return (0);
+}
