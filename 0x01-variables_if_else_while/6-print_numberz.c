653d1bd4b978ebc04553f88e96c3f40be6374b4e
chrisvanndy <chrisvannyc@gmail.com>
2021-02-05 03:19:18 +0000

    numbers with for

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main - my function
+ * numbers 0-9
+ *
+ * return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 10; x++)
+	putchar (x + '0');
+	putchar ('\n');
+	return (0);
+}
