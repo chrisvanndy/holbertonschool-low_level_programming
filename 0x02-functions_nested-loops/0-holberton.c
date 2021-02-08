d85dd892c6cadd10a776fa73ee93224abbc1ec7e
chrisvanndy <chrisvannyc@gmail.com>
2021-02-08 16:48:42 +0000

    print Holberton

---
+++ 0x02-functions_nested-loops/0-holberton.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function prints "holberton"
+ * function prints via printf()
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Holberton\n");
+
+	return (0);
+}
