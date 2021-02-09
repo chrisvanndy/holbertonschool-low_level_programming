423af865da4354d6f99b1ad7a076f4f0020f7cf0
chrisvanndy <chrisvannyc@gmail.com>
2021-02-09 16:05:37 +0000

    betty style / white space

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
 /**
+ * _isalpha - returns 1 for any alpha character
+ * returns 0 if other character
  *
- *
- * 
+ * Return: 0
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c>= 65 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 	return (1);
 	}
 	else
-	
+
 	return (0);
 }
 
