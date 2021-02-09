a247ac1559df2005a81554f5389c752907bb342b
chrisvanndy <chrisvannyc@gmail.com>
2021-02-09 15:37:20 +0000

    betty style

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_alphabet - prototyping function to write alphabet
+ * incrament a to z / print new line
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	for (; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
