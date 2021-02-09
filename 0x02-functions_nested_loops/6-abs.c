4e7fac4b9b7edf5eb16740aed4cd78c437b2c0c7
chrisvanndy <chrisvannyc@gmail.com>
2021-02-09 16:14:52 +0000

    white space / spaces

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
+ * _abs - checks absolute value of r
+ * returns the variable
  *
- *
- * 
+ * Return: r
  */
 int _abs(int r)
 {
-	if (r < 0)  
-	{ 
-        r = (-1) * r;
+	if (r < 0)
+	{
+	r = (-1) * r;
 	}
 
 	return (r);
-} 
+}
