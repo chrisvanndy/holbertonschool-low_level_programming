684e393a969f2270a7fbc3344602f8350592d795
chrisvanndy <chrisvannyc@gmail.com>
2021-02-09 15:58:28 +0000

    whitespace

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
- * 
- * 
+ * _islower - returns 1 if lowercase integer
+ * otherwise returns 0
  *
+ * Return: 0
  */
 int _islower(int c)
 {
@@ -12,6 +13,6 @@ int _islower(int c)
 	return (1);
 	}
 	else
-	
+
 	return (0);
 }
