7fcc118a2cc06bbe5040ed4b769858f415bb08ac
chrisvanndy <chrisvannyc@gmail.com>
2021-02-10 04:52:04 +0000

    logic

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c && c > b)
+	if (a > b && c > b)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c && a > c)
+	else if (b > a && b > c )
 	{
 		largest = b;
 	}
