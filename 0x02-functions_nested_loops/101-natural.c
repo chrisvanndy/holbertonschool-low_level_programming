0589387e9663107d208e0746f4fa44b4fe9a6b51
chrisvanndy <chrisvannyc@gmail.com>
2021-02-11 01:26:00 +0000

    Garrison and I are smart

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - adds two for loops
+ * add all multiples of 3 and 5 under 1024
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int sum = 0;
+	int sum2= 0;
+
+	for (a = 3; a <= 1024; a += 3)
+    		sum += a;
+
+	for (b = 5; b <=1024; b += 5)
+		sum2 += b;
+
+	printf("%d\n", sum + sum2);
+	return (0);
+}
