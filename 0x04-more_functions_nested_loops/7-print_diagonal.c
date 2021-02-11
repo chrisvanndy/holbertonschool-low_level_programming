f22d8da81ae2e08ea581b3e9834dc4a39b4cd0a4
chrisvanndy <chrisvannyc@gmail.com>
2021-02-11 23:30:48 +0000

    that was hard

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,13 +14,27 @@ void print_diagonal(int n)
 	{
 		/*printf("outside loop: x is %d, y is %d, and n is %d", x, y, n);*/
 		_putchar('\\');
+
+		if(x != n - 1)
+		{
 		_putchar('\n');
+		}
+		else
+		{
+			continue;
+		}
+		if (y != n - 1)
+		{
 		for (y = n - x; y <= n; y++)
 		{
 		/* printf("inside loop x is %d y is %d and n is %d", x, y, n);*/
 			_putchar(' ');
 		}
-	}
+		}
+		else
+		{
+			continue;
+		}
+	}	
 	_putchar('\n');
 }
-
