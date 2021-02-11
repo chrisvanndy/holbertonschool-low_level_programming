87517a7117dc61e0e681288a29eca9923522c00c
chrisvanndy <chrisvannyc@gmail.com>
2021-02-11 01:28:22 +0000

    add two strings results

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,12 +11,12 @@ int main(void)
 	int a;
 	int b;
 	int sum = 0;
-	int sum2= 0;
+	int sum2 = 0;
 
 	for (a = 3; a <= 1024; a += 3)
-    		sum += a;
+		sum += a;
 
-	for (b = 5; b <=1024; b += 5)
+	for (b = 5; b <= 1024; b += 5)
 		sum2 += b;
 
 	printf("%d\n", sum + sum2);
