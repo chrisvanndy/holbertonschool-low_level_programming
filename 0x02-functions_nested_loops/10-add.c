816bcf93d0e90ec025d4feedb3ade4f75d99132c
chrisvanndy <chrisvannyc@gmail.com>
2021-02-11 00:12:45 +0000

    simple and safe

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - function adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: x
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
