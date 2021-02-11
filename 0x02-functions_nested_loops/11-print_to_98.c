ce7b5ab36c5035f80ff3c50fd639cf1604850d01
chrisvanndy <chrisvannyc@gmail.com>
2021-02-11 03:49:36 +0000

    print 98/n not /n

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - inc or dec to 98 from given number
+ * @n: variable used to int or dec from
+ *
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	int x;
+
+	if (n > 98)
+	{
+	for (x = n; x >= 98; x--)
+	{
+	printf("%d, ", x);
+	if (x == 98)
+	{
+	printf("98\n");
+	}
+	}
+	}
+		else if (n < 98)
+		{
+		for (x = n; x <= 98; x++)
+		{
+		printf("%d, ", x);
+		if (x == 98)
+		{
+		printf("98\n");
+		}
+		}
+		}
+			else
+			printf("98\n");
+}
+
