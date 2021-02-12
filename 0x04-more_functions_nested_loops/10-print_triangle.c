d2dfe3c12db463acfc0566e7a2f18e2c8398bf29
chrisvanndy <chrisvannyc@gmail.com>
2021-02-12 03:46:51 +0000

    new live for 0

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,9 +9,14 @@ void print_triangle(int size)
 {
 	int x, y;
 
+	if (size == 0)
+	{
+	_putchar('\n');
+	}
+
 	for (x = 1; x <= size; x++)
 	{
-
+	
 		for (y = x; y < size; y++)
 		{
 			_putchar(' ');
