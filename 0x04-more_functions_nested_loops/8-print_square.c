174ae0e6112792ccde87520b6e2de13465f58d04
chrisvanndy <chrisvannyc@gmail.com>
2021-02-12 00:58:52 +0000

    \n placing

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_square - prints square with character
+ * @size: size of square line x column
+ *
+ * return: null
+ */
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < size; x++)
+	{
+					/*printf("X=%d Y=%d size= %d", x, y, size);*/
+		for (y = 0; y < size; y++)
+		{
+			_putchar('#');
+					/*printf("X=%d Y=%d size= %d", x, y, size);*/
+		}
+			_putchar('\n');
+	}
+}
