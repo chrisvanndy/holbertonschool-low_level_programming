7d3c0e8d5650fbdb4299e7890cb313e636436758
chrisvanndy <chrisvannyc@gmail.com>
2021-02-12 02:24:41 +0000

    betty style

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,29 +6,30 @@
  */
 int main(void)
 {
-        int x;
-        for (x = 1; x <= 100; x++)
-        {
-                if (x % 3 == 0 && x % 5 == 0)
-                {
-                        printf("FizzBuzz ");
-                }
-                else if (x % 5 == 0 && x != 100)
-                {
-                        printf("Buzz ");
-                }
-                else if (x % 3 == 0)
-                {
-                        printf("Fizz ");
-                }
-                else if (x == 100)
-                {
-                        printf("Buzz\n");
-                }
-                else
-                {
-                        printf("%d ", x);
-                }
-        }
+	int x;
+
+	for (x = 1; x <= 100; x++)
+		{
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 5 == 0 && x != 100)
+		{
+			printf("Buzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (x == 100)
+		{
+			printf("Buzz\n");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
 return (0);
 }
