73900036a2c14aae28f8602b8c0abdff1e9c9817
chrisvanndy <chrisvannyc@gmail.com>
2021-02-12 02:21:16 +0000

    x == 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 /**
- * main  - print fizz, buzz, and fizzbuzz in place of numbers
+ * main - replaces multiples given with fizz, buzz, and fizzbuzz
  *
  * Return: 0
  */
 int main(void)
 {
-	int x;
-
-	for (x = 1; x <= 100; x++)
-	{
-		if (x % 3 == 0 && x % 5 == 00)
-		{
-			printf("FizzBuzz ");
-		}
-		else if ((x % 3) == 0)
-		{
-			printf("Fizz ");
-		}
-		else if ((x % 5) == 0)
-		{
-			printf("Buzz ");
-		}
-		else
-		{
-			printf("%d ", x);
-		}
-	}
-
-	printf("\n");
-
-	return (0);
+        int x;
+        for (x = 1; x <= 100; x++)
+        {
+                if (x % 3 == 0 && x % 5 == 0)
+                {
+                        printf("FizzBuzz ");
+                }
+                else if (x % 5 == 0 && x != 100)
+                {
+                        printf("Buzz ");
+                }
+                else if (x % 3 == 0)
+                {
+                        printf("Fizz ");
+                }
+                else if (x == 100)
+                {
+                        printf("Buzz\n");
+                }
+                else
+                {
+                        printf("%d ", x);
+                }
+        }
+return (0);
 }
