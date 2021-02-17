36dd5646ad1f8ab23a31a194d3bcbad8aab6db09
chrisvanndy <chrisvannyc@gmail.com>
2021-02-17 17:26:05 +0000

    trimming unecessary variables

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,33 +7,28 @@
  */
 void print_rev(char *s)
 {
-	int i, l;
+	int l;
 
 	l = _strlen(s);
-	i = l;
 
-	while (i >= 0)
+	while ( l >= 0)
 	{
-		_putchar(s[i]);
-		i--;
+		_putchar(s[l]);
+		l--;
 	}
-
-	_putchar('\n');
+_putchar('\n');
 }
-/**
- * _strlen - determines characters in a string)
- * @s: s points to string
- *
- * Return: x
- */
 int _strlen(char *s)
 {
-	int x = 0;
+        int x = 0;
 
-	while (*(s + x) != '\0')
-	{
-		x++;
-	}
+        while (*(s + x) != '\0')
+        {
+                x++;
+        }
 
-	return (x);
+        return (x);
 }
+
+
+
