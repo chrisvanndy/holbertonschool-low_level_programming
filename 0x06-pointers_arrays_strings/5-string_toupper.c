261f3c61e31fbf480601d44d9c811be467273128
chrisvanndy <chrisvannyc@gmail.com>
2021-02-18 23:31:27 +0000

    lower to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * string_toupper - turns lower to upper
+ * @i: pinters to s
+ * Return: void
+ */
+char *string_toupper(char *i)
+{
+	int n;
+
+	while (i[n] != '\0')
+	{
+		if (i[n] >= 'a' && i[n] <= 'z')
+		{
+			i[n] = i[n] - 32;
+		}
+	n++;
+	}
+	return (i);
+}
