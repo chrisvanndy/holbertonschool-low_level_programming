f51000d0fbaed667e4caaa2ca12d3f10d5185f4f
chrisvanndy <chrisvannyc@gmail.com>
2021-02-19 04:00:05 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * rot13 - creates a cipher for rotating chars 13 places
+ * @str: is the string we are ciphering
+ *
+ * Return: string
+ */
+char *rot13(char *str)
+{
+	int i = 0, n;
+	char r[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char r2[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpP";
+	
+	while (str[i] != '\0')
+	{
+		for (n = 0; r[n] != '\0'; n++)
+		{
+			if (str[i] == r[n])
+			{
+				str[i] = r2[n];
+			}
+		}
+	i++;
+	}
+		return (str);
+}
