f83e74fac364e2cf28e71178bff3a07bb211424e
chrisvanndy <chrisvannyc@gmail.com>
2021-02-19 01:20:42 +0000

    hours

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ *
+ */
+char *cap_string(char *str)
+{
+	int i=0 , n;
+	char sep[13] = {32, 9, 12, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	
+	while (str[i] != '\0')
+	{
+		for (n = 0; n < 13; n++)
+		{
+			if (str[i] == sep[n])
+			{	
+				i++;
+				if (str[i] >= 'a' && str[i] <= 'z')
+				{
+					str[i] = str[i] - 32;
+				}
+				if (str[i] == ' ')
+				i++;
+				if (str[i] >= 'a' && str[i] <= 'z')
+                                {
+                                        str[i] = str[i] - 32;
+                                }
+	
+			}
+		}	
+	i++;
+	}
+	return (str);
+}
+
+
