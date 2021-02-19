23047db8ce2dbefa41f1f592cdf08639871f58a3
chrisvanndy <chrisvannyc@gmail.com>
2021-02-19 02:10:08 +0000

    trying

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,23 +9,17 @@ char *cap_string(char *str)
 {
 	int i = 0, n;
 	char sep[13] = {32, 9, 12, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-
+	
+	
 	while (str[i] != '\0')
 	{
 		for (n = 0; n < 13; n++)
 		{
 			if (str[i] == sep[n])
-			{
-				i++;
-				if (str[i] >= 'a' && str[i] <= 'z')
+			{	
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				{
-					str[i] = str[i] - 32;
-				}
-				if (str[i] == ' ')
-				i++;
-				if (str[i] >= 'a' && str[i] <= 'z')
-				{
-					str[i] = str[i] - 32;
+					str[i + 1] = str[i + 1] - 32;
 				}
 			}
 		}
