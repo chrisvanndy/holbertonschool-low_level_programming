e75ca54883452b3d494d234a44a8ebb357b9e460
chrisvanndy <chrisvannyc@gmail.com>
2021-02-19 01:24:57 +0000

    betty

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
 /**
+ * cap_string - capitolaizes characters after certain values
+ * @str: string of reference
  *
- *
- *
- *
+ * Return: string
  */
 char *cap_string(char *str)
 {
-	int i=0 , n;
+	int i = 0, n;
 	char sep[13] = {32, 9, 12, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-	
+
 	while (str[i] != '\0')
 	{
 		for (n = 0; n < 13; n++)
 		{
 			if (str[i] == sep[n])
-			{	
+			{
 				i++;
 				if (str[i] >= 'a' && str[i] <= 'z')
 				{
@@ -24,12 +24,11 @@ char *cap_string(char *str)
 				if (str[i] == ' ')
 				i++;
 				if (str[i] >= 'a' && str[i] <= 'z')
-                                {
-                                        str[i] = str[i] - 32;
-                                }
-	
+				{
+					str[i] = str[i] - 32;
+				}
 			}
-		}	
+		}
 	i++;
 	}
 	return (str);
