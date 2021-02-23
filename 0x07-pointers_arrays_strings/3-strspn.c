9011298fe66d74d4d3039945e485608d2b45dc87
chrisvanndy <chrisvannyc@gmail.com>
2021-02-23 17:24:30 +0000

    betty

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 /**
- *
- * 
- *
+ * _strspn - search prefix string in given
+ * @s: string given
+ * @accept: prefix string given for search
+ * Return: c
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, c = 0;
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
@@ -18,7 +19,7 @@ unsigned int _strspn(char *s, char *accept)
 			break;
 			}
 		}
-	if (accept[j] == '\0')	
+	if (accept[j] == '\0')
 	break;
 	}
 	return (c);
