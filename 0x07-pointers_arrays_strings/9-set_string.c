0a1b75c893aada4c0a0a3b0fe6619e72f26f34bb
chrisvanndy <chrisvannyc@gmail.com>
2021-02-23 23:44:18 +0000

    simplicty

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * set_string - function sets value of pointer to char
+ * @s: double pointer to location of string s1
+ * @to: pointer to value at s0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
