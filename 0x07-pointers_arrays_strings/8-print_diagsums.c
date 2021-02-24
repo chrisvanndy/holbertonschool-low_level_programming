95f8c5f09a635bdbaed5983eec26028be31e19e2
chrisvanndy <chrisvannyc@gmail.com>
2021-02-24 04:00:12 +0000

    one variable

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,15 +8,15 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int x, y, diag1 = 0, diag2 = 0;
+	int x, diag1 = 0, diag2 = 0;
 
 	for (x = 0; x < (size * size); x += (size + 1))
 	{
 		diag1 += a[x];
 	}
-	for (y = 0; y < (size * size) - size + 1; y += (size - 1))
+	for (x= 0; x < (size * size) - size + 1; x += (size - 1))
 	{
-		diag2 += a[y];
+		diag2 += a[x];
 	}
 	printf("%d, %d\n", diag1, diag2);
 }
