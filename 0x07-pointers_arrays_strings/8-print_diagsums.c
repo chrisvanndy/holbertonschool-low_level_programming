45105e422d88c39510b66e6bc5ac9980fa7b0979
chrisvanndy <chrisvannyc@gmail.com>
2021-02-24 04:03:38 +0000

    trying a different initialization

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,13 +8,13 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int x, diag1 = 0, diag2 = 0;
+	int x, diag1, diag2;
 
-	for (x = 0; x < (size * size); x += (size + 1))
+	for (x = 0, diag1 = 0; x < (size * size); x += (size + 1))
 	{
 		diag1 += a[x];
 	}
-	for (x= 0; x < (size * size) - size + 1; x += (size - 1))
+	for (x = 0 diag2 = 0; x < (size * size) - size + 1; x += (size - 1))
 	{
 		diag2 += a[x];
 	}
