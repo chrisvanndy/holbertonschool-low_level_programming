b6e8310999a8d3ba532494a238933c66ec300725
chrisvanndy <chrisvannyc@gmail.com>
2021-03-03 01:08:00 +0000

    string into string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - length of string
+ * @s: string
+ * Return: integer
+ */
+char _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strdup - copies string into a pointer to a string
+ * @str: string being copied into pointer to string
+ *
+ * Return: pointer to string
+ */
+char *_strdup(char *str)
+{
+	int i;
+
+	char *s;
+
+	s = (char *) malloc(_strlen(str) + sizeof(char) + 1);
+
+	if (str == NULL)
+		return (NULL);
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		s[i] = str[i];
+	}
+
+	s[i] = '\0';
+	return (s);
+}
