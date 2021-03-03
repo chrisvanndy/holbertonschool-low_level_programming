e579766aed167ac5f3f08f22c080353993b88c98
chrisvanndy <chrisvannyc@gmail.com>
2021-03-03 05:06:27 +0000

    void

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - free grid
+ * @grid: grid
+ * @height: height
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	(void) height;
+	free(grid);
+}
