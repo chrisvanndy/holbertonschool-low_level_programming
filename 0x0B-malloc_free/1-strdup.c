117d31d9e8f3153c8b2fcd88f8bf01317419766e
chrisvanndy <chrisvannyc@gmail.com>
2021-03-03 01:47:08 +0000

    account for string lenght

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,7 +6,7 @@
  * @s: string
  * Return: integer
  */
-char _strlen(char *s)
+int _strlen(char *s)
 {
 	int i = 0;
 
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,56 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - returns length of given string
+ * @s: string
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * str_concat - concatinates two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: s
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int i, ii, sz1 = _strlen(s1), sz2 = _strlen(s2);
+
+	s = (char *) malloc(sz1 + sz2 + 1);
+
+	if (s == NULL)
+		return (NULL);
+	if (s1 == NULL)
+	{
+		s1 = '\0';
+	}
+	if (s2 == NULL)
+	{
+		s2 = '\0';
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		s[i] = s1[i];
+	}
+	for (ii = 0; s2[ii] != '\0'; ii++)
+	{
+		s[i] = s2[ii];
+		i++;
+	}
+
+	s[i] = '\0';
+	return (s);
+}
