f289ecd3a1b47b2dee57dee62243901b01f230bb
chrisvanndy <chrisvannyc@gmail.com>
2021-03-03 23:43:45 +0000

    free and i--) git push

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,10 +25,17 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(width * sizeof(int));
+		grid[i] = (int *) malloc(width * sizeof(int));
 
 		if (grid[i] == NULL)
+		{
+			for(; i > 0; i--)
+			{
+				free(grid[i]);
+			}
+			free(grid);
 			return (NULL);
+		}
 	}
 	for (j = 0; j < height; j++)
 	{
