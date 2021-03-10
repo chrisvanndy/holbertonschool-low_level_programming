77d120e132d65fd9f828a947e27539e067c99aa0
chrisvanndy <chrisvannyc@gmail.com>
2021-03-10 19:24:34 +0000

    betty / spelling

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
+/**
+ * array_iterator - iterates throug array performs function subtask
+ * @array: given array
+ * @size: size_t is unsigned int (size of array)
+ * @action: function pointer given as argument
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
