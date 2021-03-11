3933d27be78e29911c07f00045587ae645030240
chrisvanndy <chrisvannyc@gmail.com>
2021-03-11 21:38:38 +0000

    less than or equal n - 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(theNumbers, n);
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n - 1; i++)
 	{
 		printf("%d", va_arg(theNumbers, int));
 
