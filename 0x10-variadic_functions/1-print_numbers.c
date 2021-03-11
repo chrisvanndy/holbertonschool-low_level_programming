ddad9182e02eb80c2ce76ff801b0e08a414c6932
chrisvanndy <chrisvannyc@gmail.com>
2021-03-11 22:11:38 +0000

    betty

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (separator != NULL && i != n - 1)
 			printf("%s", separator);
 	}
-	printf ("\n");
+	printf("\n");
 
 	va_end(theNumbers);
 }
