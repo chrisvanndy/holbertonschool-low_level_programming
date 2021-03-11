fc6a6646d964a48c3c2497ab789181581f9635b1
chrisvanndy <chrisvannyc@gmail.com>
2021-03-11 20:34:08 +0000

    variadic sums

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - takes given list of ints and sums all
+ * @n: list of
+ *
+ * Return: sum = sum of all ints in given list
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum = 0;
+
+	va_list numbers;
+
+	va_start(numbers, n);
+
+	for (i = 0; i < n; i++)
+	{
+	sum += va_arg(numbers, int);
+	}
+
+	va_end(numbers);
+
+	return (sum);
+
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,2 @@
+Read me for 0x10, variadic functions.
+
