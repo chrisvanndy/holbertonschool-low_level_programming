01eb3d3e360cfd53980efbb417557594cbafd61a
chrisvanndy <chrisvannyc@gmail.com>
2021-03-11 20:49:59 +0000

    placement of if

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,12 +13,12 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_list numbers;
 
-	va_start(numbers, n);
-
 	if (n == 0)
 	{
 		return (0);
 	}
+
+	 va_start(numbers, n);
 
 		for (i = 0; i < n; i++)
 		{
