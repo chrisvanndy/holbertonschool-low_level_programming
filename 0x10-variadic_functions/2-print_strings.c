211ff300d118a6c3e0c25d71c73a240dfd40f4ec
chrisvanndy <chrisvannyc@gmail.com>
2021-03-12 00:22:35 +0000

    betty

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-/** 
- * print_strings - takes char* arguments and prints 
+/**
+ * print_strings - takes char* arguments and prints
  * @separator: given constant string
  * @n: fixed number of arguments being passed
  * Return: void
@@ -28,7 +28,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("nil");
 		}
-		if (separator != NULL && i != n -1)
+		if (separator != NULL && i != n - 1)
 		{
 			printf("%s", separator);
 		}
