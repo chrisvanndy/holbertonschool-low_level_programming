f209a547be4a25c0296e55821bf98635aef8e86d
chrisvanndy <chrisvannyc@gmail.com>
2021-03-12 03:29:19 +0000

    (nil)

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,7 +14,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *tmp;
 
 	va_list(strings);
-	
+
 	if (n != 0)
 	va_start(strings, n);
 
@@ -27,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		else
 		{
-			printf("nil");
+			printf("(nil)");
 		}
 		if (separator != NULL && i != n - 1)
 		{
