1a95c9e1445487db6af057649cc870ab887799be
chrisvanndy <chrisvannyc@gmail.com>
2021-03-18 21:02:59 +0000

    add node at beginning of list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	
+	list_t *new;
+
+	if (str == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->len = (unsigned int)_strlen(new->str);
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
+/**
+ * _strlen - length of string
+ * @s: str
+ * Return: int (length of string in bytes)
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
