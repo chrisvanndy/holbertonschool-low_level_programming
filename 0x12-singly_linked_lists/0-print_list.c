58777ae3f1d4ce062f34b9e3b47009821adbcdeb
chrisvanndy <chrisvannyc@gmail.com>
2021-03-18 19:18:39 +0000

    task zero);

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * print_list - prints argumetns in linked list
+ * @h: pointer to node
+ *
+ * Return: size_t (i)
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h->next != NULL)
+	{
+		i++;
+		if (h->str == NULL)
+		{
+		printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+		i++;
+		if (h->str == NULL)
+		{
+		printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%u] %s\n", h->len, h->str);
+		}
+
+	return (i);
+}
