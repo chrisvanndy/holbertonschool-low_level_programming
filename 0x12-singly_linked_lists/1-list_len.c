8e5b64e515a45854fe0241acda4dcf8a74a41542
chrisvanndy <chrisvannyc@gmail.com>
2021-03-18 19:36:09 +0000

    count nodes in linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * list_len - returns number of nodes in a list
+ * @h: pointer to node
+ * Return: size_t (count)
+ */
+size_t list_len(const list_t *h);
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (count);
+	while (h->next != NULL)
+	{
+		count++
+		h = h->next;
+	}
+	count++;
+	return (count);
+}
