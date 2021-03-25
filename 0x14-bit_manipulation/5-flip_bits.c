a47ce6dd8b87df161e3732d09ba6b106948ec7d5
chrisvanndy <chrisvannyc@gmail.com>
2021-03-25 23:32:44 +0000

    flippin' bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * count_setbits - counts number of bits that are flipped
+ * @n: given int
+ * @m: given int
+ * Return: int (count)
+ */
+unsigned int count_setbits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count = 0;
+
+	while (n > 0)
+	{
+		count++;
+		n &= (n - 1);
+	}
+
+	return (count);
+}
+/**
+ * flip_bits - flips bits between two given ints
+ * @n: given int
+ * @m: given int
+ * Return: int
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	return (count_setbits(n ^ m));
+}
