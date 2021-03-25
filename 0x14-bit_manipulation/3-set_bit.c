da17535fe4729220955215a9fe409ad4a32240eb
chrisvanndy <chrisvannyc@gmail.com>
2021-03-25 23:13:56 +0000

    betty style

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * set_bit - reset value at given index to one
+ * @n: given number
+ * @index: given index
+ * Return: int
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * clear_bit - clears given bit at index
+ * @n: given number
+ * @index: given index place
+ * Return: int
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
-                return (-1);
+		return (-1);
 
-        *n &= ~(1 << index);
-                return (1);
+	*n &= ~(1 << index);
+		return (1);
 }
 
