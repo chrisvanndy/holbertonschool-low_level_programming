fb9f55c1c960fc2671518a81d4557c84bb52c6dd
chrisvanndy <chrisvannyc@gmail.com>
2021-03-25 20:05:37 +0000

    first push

---
+++ 0x14-bit-manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * binary_to_uint - returns unsigned in from binary arg
+ * @b: binary argument passed to function
+ *
+ * Return: unsigned int (b)
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result;
+
+	if (b == NULL)
+		return (0);
+	for (result = 0; *b != '\0'; b++)
+	{
+		if (*b < 48 || *b > 49)
+			return (0);
+	
+	if (*b == '1')
+		result = (result << 1) | 1;
+	else if (*b == '0')
+			result <<= 1;
+	else
+		break;
+	}
+	return (result);
+
+}
---
+++ 0x14-bit-manipulation/README.md
@@ -0,0 +1,1 @@
+This is a readme for tasks 0x14-bit-manipulation
