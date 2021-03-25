a3bee8903a0c8943d3e43ae6cd2184a6cf2bb42f
chrisvanndy <chrisvannyc@gmail.com>
2021-03-25 20:18:09 +0000

    betty style

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,13 +14,12 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (result = 0; *b != '\0'; b++)
 	{
-		if (*b < 48 || *b > 49)
-			return (0);
-	
+	if (*b < 48 || *b > 49)
+		return (0);
 	if (*b == '1')
 		result = (result << 1) | 1;
 	else if (*b == '0')
-			result <<= 1;
+		result <<= 1;
 	else
 		break;
 	}
