53c69324b0692a17dad59b6cb6eb83d96dd1a7bf
chrisvanndy <chrisvannyc@gmail.com>
2021-07-28 19:50:15 +0000

    betty

--- 0x00-hash_tables/2-key_index.c
+++ 0x00-hash_tables/2-key_index.c
@@ -3,12 +3,10 @@
  * key_index - returns the index of hashtable member
  * @key: the key * string value
  * @size: the size of the array
- * return: index place
+ * Return: index place
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
 }
-
- 
