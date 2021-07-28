7340aa25b7a33d309961995d7281bd477bd2e620
chrisvanndy <chrisvannyc@gmail.com>
2021-07-28 19:30:28 +0000

    betty

--- 0x00-hash_tables/1-djb2.c
+++ 0x00-hash_tables/1-djb2.c
@@ -1,4 +1,9 @@
 #include "hash_tables.h"
+/**
+ * hash_djb2 - apply djb2 algo to provide randomized index
+ * @str: string passed (key value)
+ * Return: pointer to the new hash table
+ */
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
