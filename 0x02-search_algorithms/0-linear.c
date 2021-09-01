c44ca47eef676e8e57866971dcf810ca7745bf0f
Chris Vanndy <2736@holbertonschool.com>
2021-09-01 10:57:51 -0500

    first commit + task 0

---
+++ 0x02-search_algorithms/0-linear.c
@@ -0,0 +1,33 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * linear_search - searches an array for give value.
+ * @array: the array to be searched.
+ * @size: the size of the array being searched.
+ * @value: the value being searched for
+ * Returns - the array index holding the value, or -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+
+	if (array != NULL)
+	{
+		size_t i;
+
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%zu] = [%i]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				return i;
+			}
+		
+		}
+	}
+
+	return (-1);
+	
+}
---
+++ 0x02-search_algorithms/README.md
@@ -0,0 +1,1 @@
+Sorting Algorithms - Weekly C project.
