e370ce0379835460f666f322d75ccecc56639c10
Chris Vanndy <2736@holbertonschool.com>
2021-09-20 15:26:36 -0500

    starting over - over thinking the problem.

--- 0x02-search_algorithms/1-binary.c
+++ 0x02-search_algorithms/1-binary.c
@@ -9,104 +9,43 @@
  * @array: the array to be searched.
  * @size: the size of the array being searched.
  * @value: the value being searched for
- * Returns - the array index holding the value, or -1
+ * Return: the array index holding the value, or -1
  */
 
 int binary_search(int *array, size_t size, int value)
 {
-	if (array != NULL)
-	{
-		size_t i;
-		size_t low = 0;
-		size_t high = (size - 1);
+	size_t start = 0, end = size - 1, mid, i;
 
-		printf("ARRAY VALUES\n");
-		printf("low = %zu\n", low);
-		printf("high = %zu\n\n", high);
+	if (array == NULL)
+		return (-1);
+
+	while (start <= end)
+	{
+		mid = (start + end) / 2;
 
 		printf("Searching in array: ");
-		for (i = low; i <= high; i++)
+
+		for (i = start; i < end; i++)
+		printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		if (array[mid] == value)
+			return (mid);
+
+		if (array[mid] < value)
 		{
+			start = mid + 1;
 
-			if (i == high)
-				printf(" %d\n", array[i]);
-			else
-				printf(" %d,", array[i]);
-		}
-		
-		printf("ENTERING THE LOOP!!\n");
-
-		while (array[low] <= array[high])
-		{
-			int C = 1;
-			size_t mid = (low + high / 2);
-
-			printf("%d LOW == %zu\n", C, low);
-			printf("%d MID == %zu\n", C,mid);
-			printf("%d HIGH == %zu\n", C, high);
-
-			sleep(5);
-
-			if (mid == high)
-			{
-				printf("Searching in array: ");
-				printf("%zu\n", mid);
-				if (value != array[mid])
-					return (-1);
-			}
-			else if (value < array[mid])
-			{
-				high = mid - 1;
-				printf("Searching in array: ");
-				for (i = low; i <= high; i++)
-				{
-					if (i == high)
-						printf(" %d\n", array[i]);
-					else
-						printf(" %d,", array[i]);
-				}
-			}
-			else if (value > array[mid])
-			{
-				printf("INSIDE v > array[mid]\n");
-				printf("array[mid] = %d\n", array[mid]);
-				printf("value = %d\n", value);
-				printf("size = %zu", size);
-				printf("low = %zu\n", low);
-				printf("mid = %zu\n", mid);
-				printf("high = %zu\n", high);
-
-				low = mid;
-			
-				
-				printf("DETERMINE MID\n");
-				printf("%zu + %zu / 2\n", low, high);
-
-				mid = ((low + high) / 2);
-				
-				printf("NEW MID = %zu\n", mid);
-				sleep(5);
-
-
-				printf("Searching in array: ");
-				for (i = low + 1; i <= high; i++)
-				{
-					if (i == high)
-						printf(" %d\n", array[i]);
-					else
-						printf(" %d,", array[i]);
-				}
-			}
-			else if (value == array[mid])
-			{
-				return mid;
-			}
-			else
+			if (start > end)
 				return (-1);
-			
-			printf("END OF LOOP");
+		}
 
+		if (array[mid] > value)
+		{
+			end = mid - 1;
+			if (end < start)
+				return (-1);
 		}
 	}
-	return (0);
+	return (-1);
 }
