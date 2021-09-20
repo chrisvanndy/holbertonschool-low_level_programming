404d843cfe60c04b51151551ad942789b9a8a85b
Chris Vanndy <2736@holbertonschool.com>
2021-09-20 15:10:24 -0500

    editing varibale types for C90

--- 0x02-search_algorithms/0-linear.c
+++ 0x02-search_algorithms/0-linear.c
@@ -15,11 +15,11 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array != NULL)
 	{
-		size_t i;
+		unsigned int i;
 
 		for (i = 0; i < size; i++)
 		{
-			printf("Value checked array[%zu] = [%i]\n", i, array[i]);
+			printf("Value checked array[%d] = [%d]\n", i, array[i]);
 			if (array[i] == value)
 			{
 				return (i);
